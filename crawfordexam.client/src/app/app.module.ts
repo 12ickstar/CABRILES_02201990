c4c160649c3852f2337bed9d16b5a466a0a8424c
Ricky Cabriles <rcabriles@expertcollege.com>
2024-07-29 22:03:24 +0800

    transfer reactive module to dependent modules

--- crawfordexam.client/src/app/app.module.ts
+++ crawfordexam.client/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,6 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
